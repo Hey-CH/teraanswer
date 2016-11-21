b212c184d0a6d28f26a3d4c12c653b7effee85be
HeyHeyHo512 <blacktomato0512@gmail.com>
2016-11-22 07:04:48 +0900

    Add files via upload

---
+++ tera54478/tera54478/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace tera54478 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            this.Shown += (s, e) => {
+                var bmp = new Bitmap(500, 500, PixelFormat.Format32bppArgb);
+                Graphics g = Graphics.FromImage(bmp);
+                g.FillRectangle(Brushes.White, g.VisibleClipBounds);
+                g.Dispose();
+                this.DrawToBitmap(bmp, new Rectangle(20, 20, this.Width, this.Height));
+                bmp.Save(@"aaa.png");
+            };
+        }
+    }
+}
