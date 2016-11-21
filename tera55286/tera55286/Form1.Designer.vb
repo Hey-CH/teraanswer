a606b0f0a91aef3c3e1af6455ffe79101e207b27
HeyHeyHo512 <blacktomato0512@gmail.com>
2016-11-22 06:57:43 +0900

    Resolved 55286
    https://teratail.com/questions/55286

---
+++ tera55286/tera55286/Form1.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_Import = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btn_Import
+        '
+        Me.btn_Import.Location = New System.Drawing.Point(12, 12)
+        Me.btn_Import.Name = "btn_Import"
+        Me.btn_Import.Size = New System.Drawing.Size(75, 23)
+        Me.btn_Import.TabIndex = 0
+        Me.btn_Import.Text = "Button1"
+        Me.btn_Import.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 41)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 21
+        Me.DataGridView1.Size = New System.Drawing.Size(260, 209)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Column1"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Column2"
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Column3"
+        Me.Column3.Name = "Column3"
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Column4"
+        Me.Column4.Name = "Column4"
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "Column5"
+        Me.Column5.Name = "Column5"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btn_Import)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btn_Import As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+End Class
---
+++ tera55286/tera55286/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Private Sub btn_Import_Click(sender As Object, e As EventArgs) Handles btn_Import.Click
+        Dim textFile1 As New OpenFileDialog()
+        textFile1.Filter = "Cursor Files|*.csv"
+        textFile1.Title = "Select a Csv File"
+
+        If (textFile1.ShowDialog() = DialogResult.OK) Then
+            Dim textFile As FileIO.TextFieldParser
+            ' --- 入力ファイルを開く
+            textFile = New FileIO.TextFieldParser(textFile1.FileName)
+            'textFile = New FileIO.TextFieldParser("C:\temp\test.csv")   ' -- デフォルト encoding は UTF8
+            ' --- デリミターをタブと定義する
+            textFile.TextFieldType = FileIO.FieldType.Delimited
+            textFile.SetDelimiters(vbTab)
+            ' --- 行を文字型配列（currentRow）に読み込み、各列を DataGridView に格納する
+            Dim currentRow As String()  ' -- 文字型配列
+            Dim myRow As Integer = 0
+            Dim myCol As Integer = 0
+            ' ---▼▼ 行ループ
+            While Not textFile.EndOfData
+                Me.DataGridView1.Rows.Add() ' -- DataGridView に新規行を追加
+                currentRow = textFile.ReadFields()  ' -- １行を文字型配列に格納
+                Dim currentColumn As String
+                ' ---▼ 列ループ：１行分の列を埋める
+                For Each currentColumn In currentRow
+                    If (myCol > 4) Then
+                        MessageBox.Show("myCol=" & myCol.ToString)
+                    End If
+                    Me.DataGridView1(myCol, myRow).Value = currentColumn
+                    myCol += 1
+                Next    ' --▲ 列ループ
+                myCol = 0
+                myRow += 1
+            End While   ' --▲▲ 行ループ
+            ' --- 入力ファイルを閉じる
+            textFile.Close()
+            ' ---
+            Me.DataGridView1.Rows.RemoveAt(0)   ' -- 先頭行は見出し行なので削除
+        End If
+    End Sub
+End Class
