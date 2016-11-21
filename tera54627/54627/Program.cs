d26ec1f42525fdebc7884c51ec9acf958116d9e6
HeyHeyHo512 <blacktomato0512@gmail.com>
2016-11-22 07:10:59 +0900

    Add files via upload

---
+++ tera54627/54627/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace _54627 {
+    class Program {
+        static void Main(string[] args) {
+            string path = @"C:\Users\BlackTomato\Desktop\SampleCode\Jellyfish.jpg";
+            Image bmp = Bitmap.FromFile(path);
+
+            var f = new Form();
+            var tb = new TextBox();
+            tb.Text = "test";
+            tb.Location = new Point(100, 100);
+            f.Controls.Add(tb);
+
+            f.Paint += (s, e) => {
+                e.Graphics.DrawImage(bmp, new Point(0, 0));
+            };
+
+            f.ShowDialog();
+        }
+    }
+}
