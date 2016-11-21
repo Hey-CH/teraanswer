bad1cc035ff0c058e4b8b5f2fa1b3eeb134ca6b7
HeyHeyHo512 <blacktomato0512@gmail.com>
2016-11-22 07:04:17 +0900

    Add files via upload

---
+++ tera52350/tera52350/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+//using Winium.Cruciatus;
+//using Winium.Cruciatus.Core;
+//using Winium.Cruciatus.Extensions;
+
+using System.Collections;
+
+namespace testxxx {
+    class Program {
+        [DllImport("user32.dll", EntryPoint = "FindWindowEx", CharSet = CharSet.Auto)]
+        static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        static void Main(string[] args) {
+            //var path = @"C:\Windows\System32\osk.exe";
+            //var proc = Process.Start(path);
+
+            //Thread.Sleep(5000);
+
+            //ウィンドウのタイトルに「メモ帳」を含むプロセスをすべて取得する
+            var hwnd = GetWindowByTitle("スクリーン キーボード");
+
+            //結果を表示する
+            foreach(IntPtr p in hwnd) {
+                var c = GetAllChildrenWindowHandles(p);
+                foreach(IntPtr cp in c) {
+                    var ccp = GetAllChildrenWindowHandles(cp);
+                    Console.WriteLine(ccp.Count);
+                }
+            }
+
+
+            Console.WriteLine("終了しました。");
+            Console.ReadLine();
+        }
+        static System.Diagnostics.Process[] GetProcessesByWindowTitle(string windowTitle) {
+            System.Collections.ArrayList list = new System.Collections.ArrayList();
+
+            //すべてのプロセスを列挙する
+            foreach(System.Diagnostics.Process p
+                in System.Diagnostics.Process.GetProcesses()) {
+                //指定された文字列がメインウィンドウのタイトルに含まれているか調べる
+                if(0 <= p.MainWindowTitle.IndexOf(windowTitle)) {
+                    //含まれていたら、コレクションに追加
+                    list.Add(p);
+                }
+            }
+
+            //コレクションを配列にして返す
+            return (System.Diagnostics.Process[])
+                list.ToArray(typeof(System.Diagnostics.Process));
+        }
+        static List<IntPtr> GetWindowByTitle(string windowTitle) {
+            List<IntPtr> list = new List<IntPtr>();
+
+            //すべてのプロセスを列挙する
+            foreach(System.Diagnostics.Process p
+                in System.Diagnostics.Process.GetProcesses()) {
+                //指定された文字列がメインウィンドウのタイトルに含まれているか調べる
+                if(0 <= p.MainWindowTitle.IndexOf(windowTitle)) {
+                    //含まれていたら、コレクションに追加
+                    list.Add(p.MainWindowHandle);
+                }
+            }
+
+            //コレクションを配列にして返す
+            return list;
+        }
+        static List<IntPtr> GetAllChildrenWindowHandles(IntPtr hParent) {
+            List<IntPtr> result = new List<IntPtr>();
+            int ct = 0;
+            IntPtr prevChild = IntPtr.Zero;
+            IntPtr currChild = IntPtr.Zero;
+            while(true) {
+                currChild = FindWindowEx(hParent, prevChild, null, null);
+                if(currChild == IntPtr.Zero) break;
+                result.Add(currChild);
+                prevChild = currChild;
+                ++ct;
+            }
+            return result;
+        }
+    }
+    public static class Ex {
+
+        public static List<AutomationElement> Find(this AutomationElement parent, Condition conditions) {
+            var l = new List<AutomationElement>();
+
+            var tage = parent.FindAll(TreeScope.Children, conditions);
+            if(tage.Count > 0) {
+                l.AddRange(tage.Cast<AutomationElement>());
+            }
+
+            var c = parent.FindAll(TreeScope.Children, Condition.TrueCondition);
+            c.Cast<AutomationElement>().ToList().ForEach(ae => l.AddRange(ae.Find(conditions)));
+
+            return l;
+        }
+    }
+}
---
+++ tera52350_2/tera52350_2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+
+namespace tera52350_2 {
+    class Program {
+        // 指定したID属性に一致するAutomationElementを返します
+        static AutomationElement FindElementById(AutomationElement rootElement, string automationId) {
+            return rootElement.FindFirst(
+                TreeScope.Element | TreeScope.Descendants,
+                new PropertyCondition(AutomationElement.AutomationIdProperty, automationId));
+        }
+
+        // 指定したName属性に一致するAutomationElementをすべて返します
+        static IEnumerable<AutomationElement> FindElementsByName(AutomationElement rootElement, string name) {
+            return rootElement.FindAll(
+                TreeScope.Element | TreeScope.Descendants,
+                new PropertyCondition(AutomationElement.NameProperty, name))
+                .Cast<AutomationElement>();
+        }
+
+        // 指定したName属性に一致するボタン要素をすべて返します
+        static IEnumerable<AutomationElement> FindButtonsByName(AutomationElement rootElement, string name) {
+            const string BUTTON_CLASS_NAME = "Button";
+            return from x in FindElementsByName(rootElement, name)
+                   where x.Current.ClassName == BUTTON_CLASS_NAME
+                   select x;
+        }
+
+        static void Main(string[] args) {
+            if(Process.GetProcessesByName("osk").Count() <= 0) {
+                Console.WriteLine($"Please launch osk.exe");
+                return;
+            }
+
+            var mainForm = AutomationElement.FromHandle(Process.GetProcessesByName("osk").First().MainWindowHandle);
+            Console.WriteLine(mainForm.Current.NativeWindowHandle);
+
+            //var btnClear = FindElementsByName(mainForm, "").First();
+            //Console.WriteLine(btnClear.Current.NativeWindowHandle);
+
+            //// ココで要素の取得ができていない
+            //var xx = btnClear.FindAll(TreeScope.Children | TreeScope.Descendants,
+            //    new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Button));
+
+            var xx = FindElementsByName(mainForm, "");
+
+
+
+            //WalkControlElements(mainForm);
+            Console.WriteLine(xx.Count());
+            //Console.WriteLine(xx.Current.NativeWindowHandle);
+
+            Console.ReadKey();
+        }
+        static void WalkControlElements(AutomationElement element) {
+            AutomationElement elementNode = TreeWalker.ControlViewWalker.GetFirstChild(element);
+            while(elementNode != null) {
+                Console.WriteLine(elementNode.Current.ControlType.LocalizedControlType);
+                WalkControlElements(elementNode);
+                elementNode = TreeWalker.ControlViewWalker.GetNextSibling(elementNode);
+            }
+        }
+    }
+}
