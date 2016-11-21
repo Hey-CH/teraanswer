13019ceea8fab6f881a138f47da91a5cb9514b1b
HeyHeyHo512 <blacktomato0512@gmail.com>
2016-11-22 07:00:57 +0900

    Add files via upload

---
+++ tera48073/tera48073/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tera48073 {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            Database.SetInitializer(new DropCreateDatabaseAlways<ObjectModel>());
+            var db = new ObjectModel();
+            var p = new Person() { ID = 0, Name = "aiueo", Comment = "com" };
+            db.Persons.Add(p);
+            db.SaveChanges();
+
+            var d = new Document() { ID = 0, Name = "doc", Value = "val", Author = p };
+            db.Documents.Add(d);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ tera48073/tera48073/Model.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+namespace tera48073 {
+    public class ObjectModel: DbContext {
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<Person> Persons { get; set; }
+    }
+    public class Document {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public Person Author { get; set; }
+    }
+    public class Person {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Comment { get; set; }
+    }
+}
